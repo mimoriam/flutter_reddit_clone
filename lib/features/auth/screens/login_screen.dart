c25597829cc24f4a1fd2516117c06bffb9c824ef
Mimo <mimoriam420@gmail.com>
2023-12-19 03:02:26 +0500

    Commit #4 - Login UI done

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reddit_clone/core/common/sign_in_button.dart';
+import 'package:flutter_reddit_clone/core/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,7 +8,48 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Center(
+          child: Image.asset(
+            Constants.logoPath,
+            height: 40,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Skip",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            const Text(
+              'Dive into anything',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.5,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                Constants.loginEmotePath,
+                height: 400,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const SignInButton(),
+          ],
+        ),
+      ),
     );
   }
 }
