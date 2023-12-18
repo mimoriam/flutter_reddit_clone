4d54d86d4ac8368313318c4b21f3c8ec69b3f306
Mimo <mimoriam420@gmail.com>
2023-12-19 02:29:27 +0500

    Commit #2 - Added color palette

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reddit_clone/theme/palette.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Reddit Clone',
+      theme: Palette.darkModeAppTheme,
       home: const SizedBox(),
     );
   }
