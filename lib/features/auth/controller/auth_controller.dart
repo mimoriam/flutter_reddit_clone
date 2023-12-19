f67a6099b708440e36a896353ce43bbc37696520
Mimo <mimoriam420@gmail.com>
2023-12-20 02:27:06 +0500

    Commit #8 - Created simple Providers using Riverpod for caching purposes

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_reddit_clone/features/auth/repository/auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authControllerProvider = Provider(
+    (ref) => AuthController(authRepository: ref.read(authRepositoryProvider)));
 
 class AuthController {
   final AuthRepository _authRepository;
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,7 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_reddit_clone/core/providers/firebase_providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+final authRepositoryProvider = Provider((ref) => AuthRepository(
+    firestore: ref.read(firestoreProvider),
+    auth: ref.read(authProvider),
+    googleSignIn: ref.read(googleSignInProvider)));
 
 class AuthRepository {
   final FirebaseFirestore _firestore;
