6a23d684b2c14772359671202df3a737ff0797a0
Mimo <mimoriam420@gmail.com>
2023-12-20 02:44:06 +0500

    Commit #9 - Firebase Google login using Providers working

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_reddit_clone/features/auth/screens/login_screen.dart';
 import 'package:flutter_reddit_clone/theme/palette.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -10,7 +11,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
