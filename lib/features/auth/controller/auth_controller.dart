fdb630aeef203017158de42fd257c069cafea4b0
Mimo <mimoriam420@gmail.com>
2023-12-20 02:18:18 +0500

    Commit #7 - Created Repository and Controller to consume in widgets

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_reddit_clone/features/auth/repository/auth_repository.dart';
+
+class AuthController {
+  final AuthRepository _authRepository;
+
+  AuthController({required AuthRepository authRepository})
+      : _authRepository = authRepository;
+
+  void signInWithGoogle() {
+    _authRepository.signInWithGoogle();
+  }
+}
