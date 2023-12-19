d075eee3838f794a2e7c13413ffe40e47f73450c
Mimo <mimoriam420@gmail.com>
2023-12-20 01:31:46 +0500

    Commit #5 - Firebase initialization using Flutterfire CLI

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/features/auth/screens/login_screen.dart';
 import 'package:flutter_reddit_clone/theme/palette.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
