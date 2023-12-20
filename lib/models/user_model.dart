34274a2478c33b5c26a7b49455355ac95141739a
Mimo <mimoriam420@gmail.com>
2023-12-21 04:32:10 +0500

    Commit #16 1/2 - Updated user model to reflect Riwaan's

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/foundation.dart';
+
 class UserModel {
   final String name;
   final String profilePic;
   final String banner;
   final String uid;
-  final bool isAuthenticated;
+  final bool isAuthenticated; // if guest or not
   final int karma;
   final List<String> awards;
-
-  const UserModel({
+  UserModel({
     required this.name,
     required this.profilePic,
     required this.banner,
@@ -16,34 +17,6 @@ class UserModel {
     required this.karma,
     required this.awards,
   });
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      (other is UserModel &&
-          runtimeType == other.runtimeType &&
-          name == other.name &&
-          profilePic == other.profilePic &&
-          banner == other.banner &&
-          uid == other.uid &&
-          isAuthenticated == other.isAuthenticated &&
-          karma == other.karma &&
-          awards == other.awards);
-
-  @override
-  int get hashCode =>
-      name.hashCode ^
-      profilePic.hashCode ^
-      banner.hashCode ^
-      uid.hashCode ^
-      isAuthenticated.hashCode ^
-      karma.hashCode ^
-      awards.hashCode;
-
-  @override
-  String toString() {
-    return 'UserModel{ name: $name, profilePic: $profilePic, banner: $banner, uid: $uid, isAuthenticated: $isAuthenticated, karma: $karma, awards: $awards,}';
-  }
 
   UserModel copyWith({
     String? name,
@@ -79,13 +52,43 @@ class UserModel {
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
-      name: map['name'] as String,
-      profilePic: map['profilePic'] as String,
-      banner: map['banner'] as String,
-      uid: map['uid'] as String,
-      isAuthenticated: map['isAuthenticated'] as bool,
-      karma: map['karma'] as int,
-      awards: map['awards'] as List<String>,
+      name: map['name'] ?? '',
+      profilePic: map['profilePic'] ?? '',
+      banner: map['banner'] ?? '',
+      uid: map['uid'] ?? '',
+      isAuthenticated: map['isAuthenticated'] ?? false,
+      karma: map['karma']?.toInt() ?? 0,
+      awards: List<String>.from(map['awards']),
     );
+  }
+
+  @override
+  String toString() {
+    return 'UserModel(name: $name, profilePic: $profilePic, banner: $banner, uid: $uid, isAuthenticated: $isAuthenticated, karma: $karma, awards: $awards)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is UserModel &&
+        other.name == name &&
+        other.profilePic == profilePic &&
+        other.banner == banner &&
+        other.uid == uid &&
+        other.isAuthenticated == isAuthenticated &&
+        other.karma == karma &&
+        listEquals(other.awards, awards);
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+    profilePic.hashCode ^
+    banner.hashCode ^
+    uid.hashCode ^
+    isAuthenticated.hashCode ^
+    karma.hashCode ^
+    awards.hashCode;
   }
 }
