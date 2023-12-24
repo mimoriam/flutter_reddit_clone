7aac2c0b2dad62688ca1bf1319ec4cc08d160901
Mimo <mimoriam420@gmail.com>
2023-12-25 02:08:11 +0500

    Commit #18 - Community List drawer added and new route for create community screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:flutter_reddit_clone/features/community/screens/create_community_screen.dart';
 import 'package:flutter_reddit_clone/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -8,5 +9,8 @@ final loggedOutRoute =
     RouteMap(routes: {'/': (_) => const MaterialPage(child: LoginScreen())});
 
 // Authenticated routes:
-final loggedInRoute =
-    RouteMap(routes: {'/': (_) => const MaterialPage(child: HomeScreen())});
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-community': (_) =>
+      const MaterialPage(child: CreateCommunityScreen()),
+});
