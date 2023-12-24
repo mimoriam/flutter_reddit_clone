dc33805a7703a8193d49ecc988755979abdbb175
Mimo <mimoriam420@gmail.com>
2023-12-25 03:21:14 +0500

    Commit #19 - Community Screen creation UI

--- lib/features/community/screens/create_community_screen.dart
+++ lib/features/community/screens/create_community_screen.dart
@@ -9,8 +9,61 @@ class CreateCommunityScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateCommunityScreenState extends ConsumerState<CreateCommunityScreen> {
+  final communityNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    communityNameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create a community'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text("Community Name"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: communityNameController,
+              decoration: const InputDecoration(
+                hintText: 'r/Community_name',
+                filled: true,
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              maxLength: 21,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+              child: const Text(
+                "Create community",
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
