2eba5eda45c6684bf984754c8190e63ac415d50c
Mimo <mimoriam420@gmail.com>
2023-12-27 02:09:36 +0500

    Commit #25 - Community profile UI

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:flutter_reddit_clone/features/community/screens/community_screen.dart';
 import 'package:flutter_reddit_clone/features/community/screens/create_community_screen.dart';
 import 'package:flutter_reddit_clone/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
@@ -13,4 +14,10 @@ final loggedInRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: HomeScreen()),
   '/create-community': (_) =>
       const MaterialPage(child: CreateCommunityScreen()),
+  // dynamic route
+  '/r/:name': (route) => MaterialPage(
+        child: CommunityScreen(
+          name: route.pathParameters['name']!,
+        ),
+      ),
 });
