6cfc8a501ecec044455ecf36e016c5285d6fd020
Mimo <mimoriam420@gmail.com>
2023-12-31 15:29:51 +0500

    Commit #35 - Add or remove moderators

--- lib/features/community/screens/mod_tools_screen.dart
+++ lib/features/community/screens/mod_tools_screen.dart
@@ -10,6 +10,10 @@ class ModToolsScreen extends StatelessWidget {
     Routemaster.of(context).push('/edit-community/$name');
   }
 
+  void navigateToAddMods(BuildContext context) {
+    Routemaster.of(context).push('/add-mods/$name');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +25,9 @@ class ModToolsScreen extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.add_moderator),
             title: const Text('Add Moderators'),
-            onTap: () => {},
+            onTap: () {
+              navigateToAddMods(context);
+            },
           ),
           ListTile(
             leading: const Icon(Icons.edit),
