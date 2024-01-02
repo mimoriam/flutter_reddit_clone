e2e61795d607e13176d7905dfb4e05dc027e32bd
Mimo <mimoriam420@gmail.com>
2024-01-03 02:59:51 +0500

    Commit #42 - Create Posts (Text/Image/Link)

--- lib/features/post/screens/add_post_screen.dart
+++ lib/features/post/screens/add_post_screen.dart
@@ -1,16 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reddit_clone/theme/palette.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
-class AddPostScreen extends ConsumerStatefulWidget {
+class AddPostScreen extends ConsumerWidget {
   const AddPostScreen({super.key});
 
-  @override
-  ConsumerState createState() => _AddPostScreenState();
-}
+  void navigateToType(BuildContext context, String type) {
+    Routemaster.of(context).push('/add-post/$type');
+  }
 
-class _AddPostScreenState extends ConsumerState<AddPostScreen> {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    double cardHeightWidth = 120;
+    double iconSize = 60;
+    final currentTheme = ref.watch(themeNotifierProvider);
+
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () => navigateToType(context, 'image'),
+            child: SizedBox(
+              height: cardHeightWidth,
+              width: cardHeightWidth,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: currentTheme.colorScheme.background,
+                elevation: 16,
+                child: Center(
+                  child: Icon(
+                    Icons.image_outlined,
+                    size: iconSize,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () => navigateToType(context, 'text'),
+            child: SizedBox(
+              height: cardHeightWidth,
+              width: cardHeightWidth,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: currentTheme.colorScheme.background,
+                elevation: 16,
+                child: Center(
+                  child: Icon(
+                    Icons.font_download_outlined,
+                    size: iconSize,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () => navigateToType(context, 'link'),
+            child: SizedBox(
+              height: cardHeightWidth,
+              width: cardHeightWidth,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: currentTheme.colorScheme.background,
+                elevation: 16,
+                child: Center(
+                  child: Icon(
+                    Icons.link_outlined,
+                    size: iconSize,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
